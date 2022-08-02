d548f4a898e29747b3c7dc13105c2a00a0ddd24d
Negin Soleiamni <negin.kdk1@gmail.com>
2022-08-02 15:11:35 +0430

    Desktop - mobile design

---
+++ asset/script/script.js
@@ -0,0 +1,26 @@
+
+
+// Scroll Reveal 
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:2500,
+    delay:400,
+    // reset:true,
+})
+
+sr.reveal(`.right-des-text,nav`)
+sr.reveal(``,{delay:1000,origin:'bottom'})
+sr.reveal(`.right-des-icon`,{origin:'left'})
+sr.reveal(`.mobile-nav`,{origin:'right'})
+
+
+
+
+
+
+
+
+
+
